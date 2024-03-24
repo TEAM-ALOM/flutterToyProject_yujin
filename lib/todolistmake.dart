a23010f4cd4d0b7e0098133c5a75d02ba2d28c67
천유진 <riapara0423@gmail.com>
2024-03-25 00:53:22 +0900

    test.1

--- lib/todolistmake.dart
+++ lib/todolistmake.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:todolist/time.dart';
 
 class todolistmake extends StatefulWidget {
   const todolistmake({super.key});
